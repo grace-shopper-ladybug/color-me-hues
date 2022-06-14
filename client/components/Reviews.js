ac3f239bf3f6e757e9748ca020512001b86f5772
Noelle Mittelstaedt <noellemittelstaedt@gmail.com>
2022-06-14 17:31:51 -0400

    Hue redux store corrected

---
+++ client/components/Reviews.js
@@ -0,0 +1,15 @@
+
+/*
+All reviews must belong to a product
+All reviews must belong to a user
+All reviews must be at least X characters
+ */
+
+// -------- imports -------->
+
+import React from "react";
+import { connect } from 'react-redux';
+
+
+// --------------- Reviews component --------------->
+export class Reviews extends React.Component {}
--- client/store/hues.js
+++ client/store/hues.js
@@ -1,64 +1,73 @@
 // -------- imports -------->
 
 import axios from 'axios'
-import history from '../history'
+import { initial } from 'lodash';
+
 
 // --------------- HUE ACTION TYPES --------------->
+
 const GET_HUES = 'GET_HUES'
 const REMOVE_HUE = 'REMOVE_HUE'
+// const NEW_HUE = 'NEW_HUE'
 
-// --------------- HUE INITIAL STATE --------------->
-const defaultHue = {}
 
-// --------------- HUE ACTION CREATORS --------------->
-export const getHues = hue => ({type: GET_HUES, hue})
-export const removeHue = () => ({type: REMOVE_HUE})
+// ------------ initial state ------------>
+
+export const initialState = [];
+
+//  ------------ ACTION CREATORS ------------>
+
+// the _ is so I can use the same getHues in my Thunks
+export const _getHues = (hues) => {
+  return {
+    type: GET_HUES,
+    hues: hues
+  }
+}
+
+export const _removeHue = (hue)  => {
+  return {
+ type: REMOVE_HUE,
+ hue: hue
+  }
+}
+
+// Potential NEW_HUE for admin users
+
+// let nextId = 0;
+// export const _newHue = (hue) => {
+//   return{
+//     type: NEW_HUE,
+//     id: nextId++,
+//     hue: hue
+//   }
+// }
+
 
 // --------------- HUE THUNK CREATORS --------------->
-export const me = () => async dispatch => {
-  try {
-    const res = await axios.get('/auth/me')
-    dispatch(getHues(res.data || defaultHue))
-  } catch (err) {
-    console.error(err)
-  }
+
+export const getHues = () => async(dispatch) => {
+  const { hue } = await axios.get('/api/hues/')
+  dispatch(getHues(hue))
 }
 
-export const auth = (email, password, method) => async dispatch => {
-  let res
-  try {
-    res = await axios.post(`/auth/${method}`, {email, password})
-  } catch (authError) {
-    return dispatch(getHues({error: authError}))
-  }
-
-  try {
-    dispatch(getHues(res.data))
-    history.push('/home')
-  } catch (dispatchOrHistoryErr) {
-    console.error(dispatchOrHistoryErr)
+export const removeHue = (hueId) => {
+  return async(dispatch) => {
+    await axios.delete(`/api/hues/${hueId}`)
+    dispatch(_removeHue(hueId))
   }
 }
-
-export const logout = () => async dispatch => {
-  try {
-    await axios.post('/auth/logout')
-    dispatch(removeHue())
-    history.push('/login')
-  } catch (err) {
-    console.error(err)
-  }
-}
-
 // --------------- HUE REDUCER --------------->
 
-export default function(state = defaultHue, action) {
+export const hueReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_HUES:
       return action.hue
     case REMOVE_HUE:
-      return defaultHue
+      return initialHue
     default:
       return state
   }
 }
+
+export default hueReducer;
