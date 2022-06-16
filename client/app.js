780009da0d66ca9f11206bc7e5575fdb48609b0c
Cindy Yueh <cindyyueh@Cindys-MacBook-Pro.local>
2022-06-16 17:13:19 -0400

    changed navbar, added a dropdown filter to filter by color, no functionality yet LOL

--- client/app.js
+++ client/app.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {Navbar} from './components'
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Storefront from './components/Storefront'
 import Routes from './routes'
