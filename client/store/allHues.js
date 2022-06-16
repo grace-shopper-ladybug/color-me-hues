3b88381bff307dc24d6ba0e26b863bd883a160ec
Kristin Lam <klam623@gmail.com>
2022-06-15 21:59:57 -0400

    created route and store for creating new hue

--- client/store/allHues.js
+++ client/store/allHues.js
@@ -1,65 +1,61 @@
-// -------- imports -------->
-
 import axios from 'axios'
 
-// --------------- HUE ACTION TYPES --------------->
-
+// --------------- ACTION TYPES --------------->
 const GET_HUES = 'GET_HUES'
-const REMOVE_HUE = 'REMOVE_HUE'
-// const NEW_HUE = 'NEW_HUE'
-
-// ------------ initial state ------------>
-
-export const initialState = []
+const CREATE_HUE = 'CREATE_HUE'
+const DELETE_HUE = 'DELETE_HUE'
 
 //  ------------ ACTION CREATORS ------------>
-
-// the _ is so I can use the same getHues in my Thunks
-export const _getHues = hues => {
+const _getHues = hues => {
   return {
     type: GET_HUES,
     hues
   }
 }
 
-export const _removeHue = hue => {
+const _createHue = hue => {
   return {
-    type: REMOVE_HUE,
+    type: CREATE_HUE,
     hue
   }
 }
 
-// Potential NEW_HUE for admin users
+const _deleteHue = hue => {
+  return {
+    type: DELETE_HUE,
+    hue
+  }
+}
 
-// let nextId = 0;
-// export const _newHue = (hue) => {
-//   return{
-//     type: NEW_HUE,
-//     id: nextId++,
-//     hue: hue
-//   }
-// }
-
-// --------------- HUE THUNK CREATORS --------------->
+// --------------- THUNK CREATORS --------------->
 
 export const getHues = () => async dispatch => {
   const {data: hues} = await axios.get('/api/hues')
   dispatch(_getHues(hues))
 }
 
-export const removeHue = hueId => {
+export const createHue = hue => {
   return async dispatch => {
-    const {data: hue} = await axios.delete(`/api/hues/${hueId}`)
-    dispatch(_removeHue(hue))
+    const {data: created} = await axios.post('/api/hues', hue)
+    dispatch(_createHue(created))
   }
 }
-// --------------- HUE REDUCER --------------->
 
-export default function(state = initialState, action) {
+export const deleteHue = hueId => {
+  return async dispatch => {
+    const {data: hue} = await axios.delete(`/api/hues/${hueId}`)
+    dispatch(_deleteHue(hue))
+  }
+}
+
+// --------------- REDUCER --------------->
+export default function(state = [], action) {
   switch (action.type) {
     case GET_HUES:
       return action.hues
-    case REMOVE_HUE:
+    case CREATE_HUE:
+      return [...state, action.hue]
+    case DELETE_HUE:
       return state.filter(hue => hue.id !== action.hue.id)
     default:
       return state
