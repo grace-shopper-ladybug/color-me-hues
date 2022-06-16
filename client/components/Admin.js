051e5ce6c6b5923adb6c4a3bec7bbe4fde7851c5
Kristin Lam <klam623@gmail.com>
2022-06-16 16:16:04 -0400

    added react-bootstrap Form and Table

--- client/components/Admin.js
+++ client/components/Admin.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {getHues} from '../store/allHues'
 import CreateHue from './CreateHue'
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
 
 class Admin extends React.Component {
+  componentDidMount() {
+    this.props.getHues()
+  }
+
   render() {
     return (
       <div>
@@ -20,9 +28,55 @@ class Admin extends React.Component {
           non-admins should not be able to send these types of requests using an
           app like Postman or Insomnia.
         </p>
+
+        <Table striped bordered hover>
+          {' '}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.hues.map(hue => {
+              return (
+                <tr>
+                  <td>{hue.id}</td>
+                  <td>{hue.emotionName}</td>
+                  <td>{hue.description}</td>
+                  <td>{hue.price}</td>
+                  <td>{hue.quantity}</td>
+                  <td>
+                    <Button>Edit</Button>
+                  </td>
+                  <td>
+                    <Button>Delete</Button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
       </div>
     )
   }
 }
 
-export default Admin
+const mapStateToProps = state => {
+  return {
+    hues: state.hues
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getHues: () => dispatch(getHues())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Admin)
