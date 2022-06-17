c77e3a1c93440ff52fd1a6db2b02b3b65dd77139
Kristin Lam <klam623@gmail.com>
2022-06-16 22:42:06 -0400

    created EditHue component

---
+++ client/components/EditHue.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {getHue, updateHue} from '../store/singleHue'
+
+class EditHue extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      emotionName: '',
+      description: '',
+      price: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  componentDidMount() {
+    this.props.getHue(this.props.match.params.hueId)
+  }
+
+  // componentDidUpdate() {
+  //   if (this.state.emotionName === '') {
+  //     this.setState({
+  //       emotionName: this.props.hue.emotionName || '',
+  //       description: this.props.hue.description || '',
+  //       price: ''
+  //     });
+  //   }
+  // }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault()
+    this.props.updateHue({...this.props.hue, ...this.state})
+  }
+
+  render() {
+    const {emotionName, description, price} = this.state
+    const {hue} = this.props
+    const {handleChange, handleSubmit} = this
+
+    return (
+      <div>
+        <p>
+          Under construction — should I render out the current listing so people
+          know what to change? Or format the form with pre-populated text.
+        </p>
+        <h1>{hue.emotionName}</h1>
+        <p>{hue.description}</p>
+        <p>Price: ${hue.price / 100}</p>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <h2>Edit Hue</h2>
+            <input
+              name="emotionName"
+              placeholder="Name"
+              onChange={handleChange}
+              value={emotionName}
+            />
+            <input
+              name="description"
+              placeholder="Description"
+              onChange={handleChange}
+              value={description}
+            />
+            <input
+              name="price"
+              placeholder="Price"
+              onChange={handleChange}
+              value={price}
+            />
+            <button type="submit">Save Changes</button>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    hue: state.hue
+  }
+}
+
+const mapDispatchToProps = (dispatch, {history}) => {
+  return {
+    getHue: hue => dispatch(getHue(hue)),
+    updateHue: hue => dispatch(updateHue(hue, history))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditHue)
