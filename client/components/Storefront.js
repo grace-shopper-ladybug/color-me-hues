7caee3793054e2a2c9a96fc07a6774c13a34b865
Kristin Lam <klam623@gmail.com>
2022-06-18 04:16:11 -0400

    removed comments, unused imports, Review component

--- client/components/Storefront.js
+++ client/components/Storefront.js
@@ -2,7 +2,6 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import AllHues from './AllHues'
-import Navbar from './Navbar'
 
 class Storefront extends React.Component {
   render() {
